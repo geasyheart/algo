31222567dc02aab81a9617097399873e30d3dca6
yuzhang <zhangy@lianzhu.com>
2021-09-09 16:29:22 +0800

    :hammer: layernorm

---
+++ python/layer_norm.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+# https://www.jb51.net/article/213383.htm
+import numpy as np
+
+a = np.array([[1, 2, 4, 1],
+              [6, 3, 2, 4],
+              [2, 4, 6, 1]])
+
+v = a.var(axis=1)  # 方差
+# print(v, a.std(axis=1))
+m = a.mean(axis=1)  # 平均值
+
+v = v.reshape(3, 1)
+m = m.reshape(3, 1)
+
+# print(e, m)
+# layer norm
+result = (a - m) / np.sqrt(v + 1e-5)
+print(result)
+
+# 另外进行引伸，为什么BN无法在RNN上应用??
+# 1. 方向不同，BN是在batch_size那一维做，另外不能代表整体的normalization
+# 2. 每个batch的长度不一样，引入mask后有影响
